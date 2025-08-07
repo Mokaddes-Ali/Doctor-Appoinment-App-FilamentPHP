932d2ab976053839512cef4fa932f5d5d652eb96
Mokaddes Ali <mokaddes.ru2000@gmail.com>
2025-08-08 05:20:40 +0600

    first commit

---
+++ database/migrations/2025_08_07_225649_create_doctors_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete()
+            // ->restrictOnDelete() age child theke delete than parent but cascadeOnDelete() from parent child auto delete
+            ->cascadeOnUpdate();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doctors');
+    }
+};
